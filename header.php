15b05aa98eed64e509a017a63fe97e0d48646b0a
Rudie Dirkx <github@hotblocks.nl>
2019-12-26 02:48:00 +0100

    -externals

--- header.php
+++ header.php
@@ -39,6 +39,5 @@ if (!isset($body_class))
 <link rel="shortcut icon" href="images/bntfavicon.ico">
 <title><?php global $title; echo $title; ?></title>
 <link rel='stylesheet' type='text/css' href='templates/classic/styles/main.css'>
-<link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
 </head>
 <body class="<?php echo $body_class; ?>">
